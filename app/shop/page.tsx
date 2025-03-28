827b7d7d7b3f4f8f881384832f4a5c43f1fab4a6
Caleb Makafui <calebsenyah@gmail.com>
2025-03-28 17:22:57 +0000

    feat: implement ShopPage component with NewArrivalsHeader for enhanced user experience

---
+++ app/shop/page.tsx
@@ -0,0 +1,12 @@
+// This is the main shop page of your application
+import { NewArrivalsHeader } from "../components/Shop/NewArrivalsHeader";
+
+export default function ShopPage() {
+  return (
+    <div>
+      <NewArrivalsHeader />
+      <h1>Welcome to the Shop Page</h1>
+      {/* Add more shop-related content here */}
+    </div>
+  );
+}
